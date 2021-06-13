161c1a0099b4be74c960fab92a8e1420a7f3d226
lrukavina <77284546+lrukavina@users.noreply.github.com>
2021-06-14 00:18:38 +0200

    Implemented articles
    Added article class and article table in database, implemented fetching articles from database

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,18 +1,21 @@
 package database;
 
+import model.Article;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Database {
 
     private static final String CONFIG_FILE = "src/database/config.properties";
 
-    private static Connection OpenConnection() {
+    private static Connection openConnection() {
         Properties properties = new Properties();
         Connection connection = null;
 
@@ -37,5 +40,26 @@ public class Database {
         } catch (SQLException throwables){
             throwables.printStackTrace();
         }
+    }
+
+    public static List<Article> fetchAllArticles() throws SQLException{
+        List<Article> articles = new ArrayList<>();
+
+        Connection connection = openConnection();
+
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM ARTICLE");
+
+        while (rs.next()){
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            Integer quantity = rs.getInt("quantity");
+            BigDecimal price = rs.getBigDecimal("price");
+
+            Article article = new Article(id, name, quantity, price);
+            articles.add(article);
+        }
+        closeConnection(connection);
+        return articles;
     }
 }
--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,7 +1,13 @@
 package main.java.sample;
 
+import database.Database;
 import javafx.fxml.Initializable;
+import model.Article;
+
+import java.math.BigDecimal;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
@@ -9,6 +15,12 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            List<Article> articles = Database.fetchAllArticles();
+            System.out.println(articles);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 }
