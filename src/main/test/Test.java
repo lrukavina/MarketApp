33fe5ae857c4b510ef3a9ac9001f1f803e56086a
lrukavina <77284546+lrukavina@users.noreply.github.com>
2021-06-18 22:30:02 +0200

    Implemented Menu bar
    Implemented menu bar and fixed issues with object that are transferring through screens

--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -1,7 +1,9 @@
 package main.test;
 
 import enumeration.ItemType;
+import enumeration.UserType;
 import model.Item;
+import model.User;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -10,6 +12,8 @@ public class Test {
     public static void main(String[] args) throws SQLException {
 
         Item item = new Item(10L, "test", ItemType.ELECTRONICS, 1, BigDecimal.valueOf(1));
+        User user = new User(1L, "adminName", "adminSurname", UserType.ADMIN, "admin","123" );
         System.out.println(item.getCode());
+        System.out.println(user);
     }
 }
